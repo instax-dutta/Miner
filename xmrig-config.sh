006bf0dc9c2509f929bc57d734a34872f01107ba
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2021-11-24 20:33:05 +0530

    changed the wallet id

--- xmrig-config.sh
+++ xmrig-config.sh
@@ -1,10 +1,5 @@
-#!/bin/sh
-sudo apt update 
-sudo apt install screen -y 
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
-tar xf hellminer_cpu_linux.tar.gz 
-./hellminer -c startum+tcp://ap.luckpool.net:3395#xnsub -u wallet.worke -p x --cpu 2
-while [1]; do
-sleep 3
-done
-sleep 999
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o sg.minexmr.com:443 -u 47rt578QwETVrUFzDD8TTFbWoXmgDseEdBdJCiz5dyetY48EfHApztYZ9EbYkTsnP57dt7Ae9fayh2srhJkQFRVMQStWwjq -k --tls --rig-id rig
