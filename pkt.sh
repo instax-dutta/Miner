c3b4b08e8d5c1b952ca41f93d5c2ad675497eb03
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2021-12-17 21:36:23 +0530

    Create pkt.sh

---
+++ pkt.sh
@@ -0,0 +1,6 @@
+sudo apt install gcc git make curl
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+git clone https://github.com/cjdelisle/packetcrypt_rs
+cd packetcrypt_rs
+~/.cargo/bin/cargo build --release
+./target/release/packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy http://pool.pkt.world http://pool.pktpool.io
