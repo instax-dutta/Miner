2295b2aae697eeaf69a69dd450aef34c02f4a070
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2021-12-17 21:30:53 +0530

    Update and rename xmrig-config.sh to pktminer.sh

---
+++ pktminer.sh
@@ -0,0 +1,3 @@
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
+chmod +x packetcrypt
+./packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy http://pool.pkt.world http://pool.pktpool.io
--- xmrig-config.sh
+++
@@ -1,5 +0,0 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
-tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
-ls
-cd xmrig-6.15.0
-./xmrig -o sg.minexmr.com:443 -u 47rt578QwETVrUFzDD8TTFbWoXmgDseEdBdJCiz5dyetY48EfHApztYZ9EbYkTsnP57dt7Ae9fayh2srhJkQFRVMQStWwjq -k --tls --rig-id rig
