0fde1aea9eec8f6ebcc4210320aaf9a8b6a93d14
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2021-12-22 11:41:09 +0530

    Update oldminer.sh

--- oldminer.sh
+++ oldminer.sh
@@ -1,3 +1,7 @@
+apt update 
+apt upgrade -y
+apt install neofetch
+neofetch
 wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
 chmod +x packetcrypt
 ./packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy http://pool.srizbi.com/ http://pool.pkt.world/ http://pool.pktpool.io/ http://pool.pkteer.com/ http://pktco.in/ http://pool-pkt.dlinodes.com:8080/
