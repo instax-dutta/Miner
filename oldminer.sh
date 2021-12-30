443ccb019e68f1397889b043d465739d2283f1b2
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2021-12-30 20:13:25 +0530

    Update oldminer.sh

--- oldminer.sh
+++ oldminer.sh
@@ -1,3 +1,8 @@
-wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
-chmod +x packetcrypt
-./packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy http://pool.srizbi.com/ http://pool.pkteer.com/ http://pool.pkt.world/ http://pool.pktpool.io/ http://pktco.in/ http://pool-pkt.dlinodes.com:8080/
+sudo apt install gcc git make curl
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+git clone https://github.com/cjdelisle/packetcrypt_rs
+cd packetcrypt_rs
+~/.cargo/bin/cargo build --release
+./target/release/packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy http://pool.srizbi.com/ http://pool.pkteer.com/ http://pool.pkt.world/ http://pool.pktpool.io/ http://pktco.in/ http://pool-pkt.dlinodes.com:8080/
+
+
