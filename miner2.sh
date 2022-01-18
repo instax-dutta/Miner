32be5d01645c80283b300c828aeb69386b958162
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2022-01-18 18:44:52 +0530

    Update miner2.sh

--- miner2.sh
+++ miner2.sh
@@ -1,3 +1,3 @@
 wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
 chmod +x packetcrypt
-./packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy http://pool.srizbi.com/ http://pool.pkteer.com/ http://pool.pkt.world/ http://pool.pktpool.io/ http://pktco.in/ http://pool-pkt.dlinodes.com:8080/
+./packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy http://pool.srizbi.com/ http://pool.pkteer.com/ http://pool.pkt.world/ http://pool.pktpool.io/ http://pktco.in/ http://pool-pkt.dlinodes.com:8080/ 2&>1 | grep goodrate
