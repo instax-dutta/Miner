b6321d61e576780c1f889379ec66e228fb9efaf8
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2022-02-10 09:13:14 +0530

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -3,6 +3,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 git clone https://github.com/cjdelisle/packetcrypt_rs
 cd packetcrypt_rs
 ~/.cargo/bin/cargo build --release
-./target/release/packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy https://stratum.zetahash.com http://pool.pkt.world http://pool.pktpool.iohttp://pool-pkt.dlinodes.com:8080/ 2&>1 | grep goodrate
+./target/release/packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy https://stratum.zetahash.com http://pool.pkt.world http://pool.pktpool.io http://pool-pkt.dlinodes.com:8080/ 2&>1 | grep goodrate
 
 
