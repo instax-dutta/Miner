6fe2da50209a4080ecb0d3d32037d6d78d0bb5fc
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2022-03-08 20:19:45 +0530

    Updated pool order choice

--- miner.sh
+++ miner.sh
@@ -26,13 +26,15 @@ if [ "${choice}" == "y" ]; then
     sudo apt install gcc git make curl
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     git clone https://github.com/cjdelisle/packetcrypt_rs
-    cd packetcrypt_rs
-    ~/.cargo/bin/cargo build --release
     echo "completed compiling the miner !!"
+    echo "Do you want to start the miner ? [y/n]"
     read -p "Do you want to start the miner ? [y/n]" choice1
     if [ "${choice1}" == "y" ]; then
+        read -p "Enter the pool order you want to use : " POOL
         echo "Starting the miner..."
-        ./target/release/packetcrypt ann -p ${WALLET} http://pool.pkt.world/ http://pool.chymera.it/ http://pool.pktpool.io/ http://pktco.in/ http://pool.pkteer.com/ http://pool-pkt.dlinodes.com/ -t${THREAD} 2&>1 | grep goodrate 
+        cd packetcrypt_rs
+        ~/.cargo/bin/cargo build --release
+        ./target/release/packetcrypt ann -p ${WALLET} ${POOL} -t${THREAD}  
     else
         echo "Exiting..."
     fi
