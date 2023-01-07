470baf1a3f59202118e5e662f9fd25a62c05ca1e
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2023-01-07 19:43:41 +0530

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -5,4 +5,4 @@ read -p "How many Threads you want to give to the miner ?" THREAD
 read -p "Enter the pool order you want to use : " POOL
 echo "Starting the miner..."
 cd packetcrypt_rs
-./target/release/packetcrypt ann -p ${WALLET} ${POOL} -t${THREAD} 2&>1 | grep goodrate
+./target/release/packetcrypt ann -p ${WALLET} ${POOL} -t${THREAD}
