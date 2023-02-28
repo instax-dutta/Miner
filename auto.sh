6e7cc013050cb30bca3ad607513f8371433edc0b
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2023-02-28 12:28:49 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,3 +1,3 @@
 wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
 chmod +x packetcrypt
-./packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy http://pool.pktpool.io/diff/8192 http://pool.pkt.world/ http://pool.pkteer.com/
+./packetcrypt ann -p pkt1qa0qs2v6cfhdrrrspwn8gm8ccj8ts6yfaf7suhy http://pool.pktpool.io/diff/32768 https://stratum.zetahash.com/ http://pool.pkt.world/ http://pool.pkteer.com/
